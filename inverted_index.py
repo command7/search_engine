4ebba4e310eadcb78991f10916f6a1d379a7395e
Vijay Raj Saravanan Radhakrishnan <vijay_saravanan@outlook.com>
2019-03-01 21:41:51 -0500

    Feat: Add pre_process() to tokenize, filter and stem words

--- inverted_index.py
+++ inverted_index.py
@@ -1,16 +1,28 @@
 import numpy as np
-from nltk.tokenize import word_tokenize
+import nltk
+#nltk.download() #!!!Run this the first time you run your script!!!
 import os
+from string import punctuation
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
 
-test_filename = "bbc/business/001.txt"
+
+test_filename = "test.txt"
 class InvertedIndex:
     def __init__(self):
         self.documents = list()
         self.terms = None
         self.posting_lists = None
 
-    def parse_document(self, document_contents):
+    def parse_document(self, file_name):
+        document_text = self.read_text_file(file_name)
+        self.add_document(document_text)
+        processed_tokens = self.pre_process(document_text)
+        print(processed_tokens)
         pass
+
+    def add_document(self, document_content):
+        self.documents.append(document_content)
 
     def read_text_file(self, filename):
         current_dir = os.getcwd()
@@ -18,12 +30,14 @@ class InvertedIndex:
         with open(doc_path, "r") as doc:
             whole_doc = doc.read()
         return whole_doc
-        #     self.documents.append(whole_doc)
-        # return
 
-    def pre_process(self):
-
-        pass
+    def pre_process(self, document_content):
+        stop_words = set(stopwords.words('english') + list(punctuation))
+        tokens = nltk.word_tokenize(document_content)
+        filtered_words = [word.lower() for word in tokens if not word in stop_words]
+        stemmer = PorterStemmer()
+        stemmed_words = [stemmer.stem(word) for word in filtered_words]
+        return stemmed_words
 
     def update_inv_index(self):
         pass
@@ -45,5 +59,5 @@ class SearchEngine:
 
 if __name__ == "__main__":
     inv_index = InvertedIndex()
-    inv_index.read_text_file(test_filename)
+    inv_index.parse_document(test_filename)
     print(inv_index.documents)
